926d7b835fbcf1e26f3dd5ab3d81005d7c382a7e
Nawa Aripurna <nap.aripurna@gmail.com>
2024-03-15 19:21:50 +0800

    move VCR files to fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,6 +26,6 @@ module ActiveSupport
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'test/fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
