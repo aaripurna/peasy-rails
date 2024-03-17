25c8f79f8849d00736df67fd46626bce2d73ab03
Nawa Aripurna <nap.aripurna@gmail.com>
2024-03-17 12:06:01 +0700

    update user count assert

--- test/system/homepage_test.rb
+++ test/system/homepage_test.rb
@@ -14,15 +14,30 @@ class HomepageTest < ApplicationSystemTestCase
     assert_text 'NAME AGE GENDER CREATED AT ACTION'
 
     assert_equal 20, all('#user-table tbody tr').size
-    find('#male-count-text').assert_text '11'
+    daily_record = DailyRecord.first
+    find('#male-count-text').assert_text daily_record.male_count
+    find('#female-count-text').assert_text daily_record.female_count
+    find('#male-avg-age-text').assert_text daily_record.male_avg_age.round(2)
+    find('#female-avg-age-text').assert_text daily_record.female_avg_age.round(2)
   end
 
   test 'delete user' do
     visit root_path(limit: 30)
     first('#user-table tbody tr a').click
     accept_alert 'Are you sure want to delete this?'
+    daily_record = DailyRecord.first
+    find('#male-count-text').assert_text daily_record.male_count
+    find('#female-count-text').assert_text daily_record.female_count
+    find('#male-avg-age-text').assert_text daily_record.male_avg_age.round(2)
+    find('#female-avg-age-text').assert_text daily_record.female_avg_age.round(2)
+
     sleep 1
     visit root_path
     assert_equal 19, all('#user-table tbody tr').size
+    daily_record = DailyRecord.first
+    find('#male-count-text').assert_text daily_record.male_count
+    find('#female-count-text').assert_text daily_record.female_count
+    find('#male-avg-age-text').assert_text daily_record.male_avg_age.round(2)
+    find('#female-avg-age-text').assert_text daily_record.female_avg_age.round(2)
   end
 end
